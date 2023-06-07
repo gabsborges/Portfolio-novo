f2ebb5132fdfc3f99399cd2c8aecbdb4fd4be3e9
gabsborges <gabriel.borges_96@hotmail.com>
2023-06-07 11:41:27 -0300

    feat: header initialized

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,36 @@ const HeaderBar = styled.div`
     width: 80%;
     padding: 30px 0;
     margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const HeaderMenuUl = styled.ul`
+    display: flex;
+    list-style: none;
+    gap: 38px;
+`
+
+const HeaderMenuLi = styled.li`
+
+`
+
+const HeaderMenuA = styled.a`
+    color: white;
+    text-decoration: none;
 `
 
 export function Header() {
     return (
         <HeaderBar>
             <LogoBranca/>
+            <HeaderMenuUl>
+                <HeaderMenuLi><HeaderMenuA href="">Sobre Mim</HeaderMenuA></HeaderMenuLi>
+                <HeaderMenuLi><HeaderMenuA href="">Conhecimentos</HeaderMenuA></HeaderMenuLi>
+                <HeaderMenuLi><HeaderMenuA href="">Projetos</HeaderMenuA></HeaderMenuLi>
+                <HeaderMenuLi><HeaderMenuA href="">Contato</HeaderMenuA></HeaderMenuLi>
+            </HeaderMenuUl>
         </HeaderBar>
     )
 }
